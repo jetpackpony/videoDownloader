f0d60cca9f93ac888d5af8634bb0e78aa8cc7b51
jetpackpony <jetpackpony@gmail.com>
2023-11-22 18:55:05 +0300

    Add async handlers for routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import asyncHandler from "express-async-handler";
 
 const port = 4000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+app.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    res.send("Hello world");
+  }),
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
